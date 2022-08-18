6279c0cf6a96406900238020c90f42c1cac0c78c
valentin noves <novesvalentin@gmail.com>
2022-08-18 18:33:35 +0200

    remove variables

--- src/constants/dummyData.js
+++
@@ -1,6 +0,0 @@
-const tokenUrl = "https://g4ile9uo3g.execute-api.us-east-1.amazonaws.com/ForgeData";
-const apiKey = "tHTT648qkj1ncyDH5Aey75izXMv6jBMe4WQK3YUz";
-const urn = "dXJuOmFkc2sud2lwcHJvZDpmcy5maWxlOnZmLjR4eGtOZVVyVFRPbV9KMkhWQTdCRGc_dmVyc2lvbj0x";
-const mapboxToken = "pk.eyJ1IjoiZGhlcm95aW5nIiwiYSI6ImNrcHI4bzZjazA0cGMycHA4ZDVwcTk1dmUifQ.uio2clNCmAHseSbfKV1nYA";
-
-export {tokenUrl, apiKey, urn, mapboxToken};
